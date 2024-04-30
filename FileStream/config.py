a0a755fad1cadf1a755237bee8b477cd5ab54d43
jasoor07 <123425119+jasoor07@users.noreply.github.com>
2024-05-01 01:02:19 +0500

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -1,28 +1,27 @@
 from os import environ as env
-from os import getenv, environ
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Telegram:
-    API_ID = int(getenv('API_ID', '3416031'))
-    API_HASH = str(getenv('API_HASH', '9ef031429a589839341996e28b37bc33'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6841881665:AAHgnn6Kfa2NzkqYCmMh-eTMf6mfudi4bQk'))
-    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
+    API_ID = int(env.get("API_ID"))
+    API_HASH = str(env.get("API_HASH"))
+    BOT_TOKEN = str(env.get("BOT_TOKEN"))
+    OWNER_ID = int(env.get('OWNER_ID', '364399268'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL', "mongodb+srv://humbergridanupelomarid:zkaKw3LW90EiWygO@cluster0.5aqpdug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "MYAnimeIR"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'test'))
-    FORCE_SUB_ID = env.get('FORCE_SUB_ID', '-1001670816746')
+    DATABASE_URL = str(env.get('DATABASE_URL'))
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
+    SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
+    FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', True)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
-    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/388a6ddebf15ed8036919.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/388a6ddebf15ed8036919.jpg")
+    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
+    START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", '-1002071250041'))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", '-1002071250041'))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
